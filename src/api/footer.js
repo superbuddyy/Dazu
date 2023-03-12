178719c96c35159dd01343e924ed55792b533aad
nubby0208 <109526476+gruTonubby@users.noreply.github.com>
2023-03-12 11:36:45 +1000

    refix

--- src/api/footer.js
+++ src/api/footer.js
@@ -5,6 +5,14 @@ class FooterResource extends Resource {
   constructor() {
     super('admin/footer');
   }
+
+  update(id, resource) {
+    return request({
+      url: '/' + this.uri + '/' + id,
+      method: 'post',
+      data: resource,
+    });
+  }
 }
 
 export { FooterResource as default };
